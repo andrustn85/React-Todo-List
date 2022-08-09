6db2c4197cf2e39de515557379adf55ca7d0c20e
Andres R <77374408+andrustn85@users.noreply.github.com>
2022-08-09 14:11:01 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import './App.css';
 
-import React from "react";
+import React, { useState} from "react";
+
 
 function App() {
+
+const [task, setNewTask] = useState("");
+
+const [items, newItems] = useState([]);
+
+function handleTask(event) {
+  const newTask = event.target.value;
+  setNewTask(newTask)
+}
+
+function addNewTask() {
+newItems(prevItems => {
+  return [...prevItems, task];
+})
+setNewTask("");
+}
+
+
   return (
     <div className="container">
       <div className="heading">
-        <h1>To-Do List</h1>
+        <h1> To-Do List </h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input
+         type="text"
+         placeholder="Write your task here"
+         onChange={handleTask}
+         value={task}
+          />
+        <button onClick={addNewTask}>
           <span> Add </span>
         </button>
       </div>
       <div>
         <ul>
-          <li> A Item </li>
+        {items.map(todoItem => {
+          return <li> {todoItem} </li>;
+        })}
         </ul>
       </div>
     </div>
